6a2e7f090f9166175a2e62bf8efd7c3439a97c8a
unknown <bettercallguts@gmail.com>
2024-11-24 07:53:45 +0300

    initializing the base html and product model

--- Back_end/core/models.py
+++ Back_end/core/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+
+class Product(models.Model):
+    # Main
+    name = models.CharField (max_length=250)
+    Price= models.FloatField(default=0)
+    descr= models.TextField (blank=True, null=True)
+    thumb= models.ImageField(upload_to="thumbnails")
+
+
+    # 
+    quantity     = models.FloatField  (default=0)
+    out_of_stock = models.BooleanField(default=True)
+    on_sale      = models.BooleanField(default=False)
+    old_price    = models.FloatField  (default=0, )
+
+    def __str__(self) :
+        return self.name
+    
+
+class ProductImages(models.Model):
+    prod   = models.ForeignKey(Product, on_delete=models.CASCADE)
+    pass
